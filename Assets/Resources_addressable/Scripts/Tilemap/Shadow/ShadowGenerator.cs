966553e26ac72fc5288e910af4eb9df9e4c01d5c
MWHLS <asd123pwj@gmail.com>
2024-09-05 20:11:30 +0800

    v0.17.2 从碰撞体中生成阴影

---
+++ Assets/Resources_addressable/Scripts/Tilemap/Shadow/ShadowGenerator.cs
@@ -0,0 +1,53 @@
+// ref: https://diegogiacomelli.com.br/unitytips-shadowcaster2-from-collider-component/
+
+using UnityEngine;
+using UnityEngine.Tilemaps;
+using UnityEngine.Rendering.Universal;
+using System.Reflection;
+
+public class ShadowGenerator : MonoBehaviour{
+    public Tilemap tilemap;
+    GameObject _self;
+    static readonly FieldInfo _meshField;
+    static readonly FieldInfo _shapePathField;
+    static readonly MethodInfo _generateShadowMeshMethod;
+
+    static ShadowGenerator(){
+        _meshField = typeof(ShadowCaster2D).GetField("m_Mesh", BindingFlags.NonPublic | BindingFlags.Instance);
+        _shapePathField = typeof(ShadowCaster2D).GetField("m_ShapePath", BindingFlags.NonPublic | BindingFlags.Instance);
+
+        _generateShadowMeshMethod = typeof(ShadowCaster2D)
+                                    .Assembly
+                                    .GetType("UnityEngine.Rendering.Universal.ShadowUtility")
+                                    .GetMethod("GenerateShadowMesh", BindingFlags.Public | BindingFlags.Static);
+    }
+
+
+    void Start(){
+        _self = this.gameObject;
+        GameObject container = new GameObject("Container_ShadowCasters");
+        container.transform.SetParent(_self.transform);
+
+        CompositeCollider2D collider = tilemap.GetComponent<CompositeCollider2D>();
+        for (int i = 0; i < collider.pathCount; i++){
+            Vector2[] path_vertices = new Vector2[collider.GetPathPointCount(i)];
+            Vector3[] points = new Vector3[collider.GetPathPointCount(i)];
+            collider.GetPath(i, path_vertices);
+
+            for (int j = 0; j < points.Length; j++){
+                points[j] = new Vector3(path_vertices[j].x, path_vertices[j].y, 0);
+            }
+
+            GameObject obj = new("ShadowCasters_" + i);
+            obj.transform.SetParent(container.transform);
+
+            ShadowCaster2D shadowCaster = obj.AddComponent<ShadowCaster2D>();
+            shadowCaster.selfShadows = true;
+            _shapePathField.SetValue(shadowCaster, points);
+            _meshField.SetValue(shadowCaster, new Mesh());
+            _generateShadowMeshMethod.Invoke(shadowCaster, new object[] { _meshField.GetValue(shadowCaster), _shapePathField.GetValue(shadowCaster) });
+
+        }
+    }
+}
+
